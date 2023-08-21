c5d052174b280965ef97abe2d74e3a5d426d57ba
Melih Can KURU <melihcaan.99@gmail.com>
2023-08-21 15:01:56 +0300

    refactor

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -2,6 +2,11 @@ package com.hoaxify.ws.user;
 
 import jakarta.persistence.*;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @Entity
 @Table(name = "users")
@@ -11,7 +16,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min = 4, max = 20)
     private String username;
+    @NotNull
+    @Size(min = 4, max = 20)
     private String displayName;
+    @NotNull
+    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 }
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -6,11 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,16 +23,22 @@ public class UserController {
 
     @PostMapping("/api/1.0/users")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<?> createUser(@RequestBody User user){
-        String username = user.getUsername();
-        if (username == null || username.isEmpty()) {
-            ApiError apiError = new ApiError(400, "Validation Error", "/api/1.0/users");
-            Map<String, String> validationErros = new HashMap<>();
-            validationErros.put("username", "username cannot be null");
-            apiError.setValidationErrors(validationErros);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
-        }
+    public ResponseEntity<?> createUser(@Valid @RequestBody User user){
         userService.createUser(user);
         return ResponseEntity.ok("user created");
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+        ApiError apiError = new ApiError(400, "Validation Error", "/api/1.0/users");
+        Map<String, String> validationErrors = new HashMap<>();
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()){
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        apiError.setValidationErrors(validationErrors);
+        return apiError;
+    }
 }
+
+
